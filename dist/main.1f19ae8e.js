4cf1748fd2deff76373f8278baf17b108a1cb570
gracejoo26 <gracejoo26@gmail.com>
2022-10-18 22:14:51 +0900

    comparison_poerator,Logical_operator

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -119,11 +119,36 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"main.js":[function(require,module,exports) {
 //산술연산자 (arithmetic operator +,-,*,/)
-var a = 2; // = 할당연산자
+//let a = 2 // = 할당연산자
 //a = a + 1
+//a /= 1
+console.log(a); //비교연산자 (conparison operator)
 
-a /= 1;
+/* const a = 1
+const b = 3 */
+
+/* console.log(a === b) 
+
+function isEqual(x,y){
+    return x === y
+}
+
+
+console.log(isEqual( 1, 1 ))
+console.log(isEqual( 2, '2' ))
+ */
+//console.log(a < b)
+//논리 연산자 (logical operator)
+
+var a = 1 === 123;
+var b = 'AB' === 'ABc';
+var c = false;
 console.log(a);
+console.log(b);
+console.log(c);
+console.log('&&: ', a && b && c);
+console.log('||: ', a || b || c);
+console.log('!: ', !a); //not 연산자
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- main.js
+++ main.js
@@ -1,7 +1,39 @@
 //산술연산자 (arithmetic operator +,-,*,/)
 
-let a = 2 // = 할당연산자
+//let a = 2 // = 할당연산자
 //a = a + 1
-a /= 1
+//a /= 1
 
 console.log(a)
+
+
+//비교연산자 (conparison operator)
+
+/* const a = 1
+const b = 3 */
+
+/* console.log(a === b) 
+
+function isEqual(x,y){
+    return x === y
+}
+
+
+console.log(isEqual( 1, 1 ))
+console.log(isEqual( 2, '2' ))
+ */
+//console.log(a < b)
+
+//논리 연산자 (logical operator)
+
+const a = 1 === 123
+const b = 'AB' === 'ABc'
+const c = false
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+console.log('&&: ', a && b && c)
+console.log('||: ', a || b || c)
+console.log('!: ',!a) //not 연산자
